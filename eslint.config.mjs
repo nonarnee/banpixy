0203ff726b5eef38d65358ac72d3ea8d8508328a
nonarnee <nonarnee@gmail.com>
2025-03-06 03:12:12 +0900

    Lint 설정

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+import { FlatCompat } from '@eslint/eslintrc';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,7 +11,35 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  {
+    rules: {
+      'react/react-in-jsx-scope': 'off', // auto import
+      'import/order': [
+        'error',
+        {
+          groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
+          'newlines-between': 'always',
+        },
+      ],
+      'quotes': ['error', 'single'],
+      'semi': ['error', 'always'],
+      '@typescript-eslint/explicit-module-boundary-types': 'warn',
+      '@typescript-eslint/no-explicit-any': 'warn',
+      'react/prop-types': 'off',
+      '@typescript-eslint/no-unused-vars': 'warn',
+      '@typescript-eslint/consistent-type-definitions': ['error', 'interface'],
+      'import/prefer-default-export': 'warn',
+    },
+    overrides: [
+      {
+        files: ['*.jsx', '*.tsx'],
+        rules: {
+          '@typescript-eslint/explicit-module-boundary-types': 'off',
+        },
+      },
+    ],
+  },
 ];
 
 export default eslintConfig;
